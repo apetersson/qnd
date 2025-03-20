8ff80f1f783d3b9600ab9e39a0688cac8e214951
Andreas Petersson <andreas@capacity.at>
2025-03-20 21:06:08 +0100

    update title

--- pmp/src/main.tsx
+++ pmp/src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PolytopiaMarketPlanner from "./components/PolytopiaMarketPlanner";
 
-export const LINKS = [
-  {path: '/riotCalc', name: 'Riot Probability Calculator'},
-  {path: '/pmp', name: 'Polytopia Market Planner'},
-];
-
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
@@ -19,8 +14,6 @@ root.render(
     <BrowserRouter basename={base}>
       <Routes>
         <Route path="/" element={<PolytopiaMarketPlanner/>}/>
-        {/*<Route path="/riotCalc" element={<RiotProbabilityCalculator/>}/>*/}
-        {/*<Route path="/pmp" element={<PolytopiaMarketPlanner/>}/>*/}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
